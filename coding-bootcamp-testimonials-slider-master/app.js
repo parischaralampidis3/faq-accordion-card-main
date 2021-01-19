972e7a882a8905e2578928f93e6e62f16312fcd8
Paris Charalampidis <parou.char@gmail.com>
2021-01-19 07:55:59 +0200

    changes made

--- coding-bootcamp-testimonials-slider-master/app.js
+++ coding-bootcamp-testimonials-slider-master/app.js
@@ -1,1 +1,31 @@
+//refer buttons from html
+const forwards = document.getElementById("forward");
+const backward = document.getElementById("backwards");
 
+//refer div elements from html
+
+const blueColor = document.getElementById("blue");
+const redColor = document.getElementById("red");
+
+//set event listeners
+forwards.addEventListener("click", forwardButton);
+backward.addEventListener("click", backButton);
+
+//set counter
+let counter = 0;
+
+//set functions for listeners
+function forwardButton() {
+  if (counter == 0) {
+    blueColor.style.display = "block";
+    redColor.style.display = "none";
+  }
+}
+
+function backButton() {
+  if (counter <= 0) {
+    redColor.style.display = "block";
+    blueColor.style.display = "none";
+  }
+  //console.log("back button hit");
+}
