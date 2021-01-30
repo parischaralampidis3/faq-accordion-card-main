dbe1e2be70de8aee4764959e5fbb0db172f6cb57
Paris Charalampidis <parou.char@gmail.com>
2021-01-31 00:46:50 +0200

    changes made

--- coding-bootcamp-testimonials-slider-master/app.js
+++ coding-bootcamp-testimonials-slider-master/app.js
@@ -1,31 +1,31 @@
 //refer buttons from html
-const forwards = document.getElementById("forward");
-const backward = document.getElementById("backwards");
-
 //refer div elements from html
-
-const blueColor = document.getElementById("blue");
-const redColor = document.getElementById("red");
+const femaleContainer = document.querySelector(".female");
+const maleContainer = document.querySelector(".male");
+const forwardButton = document.querySelector(".btn-forward");
+const backwardButton = document.querySelector(".btn-backward");
 
 //set event listeners
-forwards.addEventListener("click", forwardButton);
-backward.addEventListener("click", backButton);
+forwardButton.addEventListener("click", setForwardButton);
+backwardButton.addEventListener("click", setBackwardButton);
 
 //set counter
 let counter = 0;
 
 //set functions for listeners
-function forwardButton() {
+function setForwardButton() {
   if (counter == 0) {
-    blueColor.style.display = "block";
-    redColor.style.display = "none";
+    femaleContainer.style.display = "block";
+    maleContainer.style.display = "none";
   }
+}
+function setBackwardButton() {
+  maleContainer.style.display = "block";
+  femaleContainer.style.display = "none";
 }
 
-function backButton() {
-  if (counter <= 0) {
-    redColor.style.display = "block";
-    blueColor.style.display = "none";
-  }
-  //console.log("back button hit");
-}
+//test
+console.log("forward button :", forwardButton);
+console.log("backward button : ", backwardButton);
+console.log("female container :", femaleContainer);
+console.log("male container :", maleContainer);
